ffad0f10c47e61b5f149819525e55711037ee197
Mark Douma <mark@markdouma.com>
2013-09-18 14:48:20 -0400

    BUG: Added example code (along with comments) to the ViewController class which demonstrates the problem with defining JotShortcut's NSString `shortDescription` and `key` properties as `assign`: it will (should and does here) cause a crash on app launch because the JotShortcut class does not retain the strings properly.

--- Example/JotTouchExample/ViewController.m
+++ Example/JotTouchExample/ViewController.m
@@ -46,6 +46,69 @@
                                     repeatRate:100]];
     
 
+	/* The following code demonstrates the problem with defining JotShortcut's
+	 `shortDescription` and `key` properties as `assign`.
+    
+	 Defining the NSString `shortDescription` and `key` properties as `assign` is dangerous;
+	 they should be defined as `copy`, `strong`, or `retain` so that the string cannot be
+	 deallocated from underneath you.
+	 
+	 The example code you've written in ViewController above that creates the JotShortcuts works
+	 just fine (out of pure luck) because of an implementation detail: when you use the @ sign
+	 to create an NSString literal, it creates an instance of a special type of NSString,
+	 __NSCFConstantString, that is allocated immediately at runtime and can never be
+	 deallocated. Take the following code for example:
+	 
+		 [jotManager addShortcutOptionButton2Default: [[JotShortcut alloc]
+										initWithShortDescription:@"Redo"
+										key:@"redo"
+										target:canvasView selector:@selector(redo)
+										repeatRate:100]];
+
+	 The strings @"Redo" and @"redo" are created at application launch and will persist
+	 indefinitely until the program terminates: they can never and will never be deallocated. For that
+	 reason, in this particular context, your implementation "works" fine. But imagine a developer
+	 doing something like in the following code:
+	 
+	 
+		for (NSUInteger i = 0; i < 10; i++) {
+			JotShortcut *shortcut = [[JotShortcut alloc] initWithShortDescription:[NSString stringWithFormat:@"Set Color %lu", (unsigned long)i]
+																			  key:[NSString stringWithFormat:@"setColor%lu", (unsigned long)i]
+																		   target:nil
+																		 selector:NSSelectorFromString([NSString stringWithFormat:@"setColor%lu", (unsigned long)i])
+																	   repeatRate:100.0];
+			if (shortcut) [jotManager addShortcutOption:shortcut];
+			
+		}
+
+	 While perhaps a contrived example, this is perfectly legal code that your SDK ought to be able to handle.
+	 
+	 Under ARC, NSString's +stringWithFormat: will be creating "ordinary" NSStrings which differ from the
+	 special __NSCFConstantString string literals created using the @ construct: they can and will be
+	 released (and subsequently deallocated) unless you specify otherwise. By specifying that JotShortcut's
+	 `shortDescription` and `key` properties be `assign` in nature, creating the JotShortcut object will not
+	 cause the strings to be retained, and the strings will be released (and deallocated) at the end of each
+	 time through the loop. You're left with 10 shortcut objects which have 2 invalid pointers each. Any attempt
+	 to subsequently access the `shortDescription` or `key` properties elsewhere in your code will likely cause
+	 a crash or unexpected behavior.
+	 
+	 Under MRC, NSString's +stringWithFormat: will be creating autoreleased strings, which, because you
+	 defined the `shortDescription` and `key` properties as `assign`, will not be retained by the
+	 JotShortcut object, and any attempt to access them at a later time (after the autorelease pool
+	 has been popped) will equate to an attempt to access a dangling pointer, likely causing a crash.
+	 
+	 Thus, unless it is commented out, the following code will likely cause a crash due to trying to send a 
+	 message to a deallocated object. */
+	
+	for (NSUInteger i = 0; i < 10; i++) {
+		JotShortcut *shortcut = [[JotShortcut alloc] initWithShortDescription:[NSString stringWithFormat:@"Set Color %lu", (unsigned long)i]
+																		  key:[NSString stringWithFormat:@"setColor%lu", (unsigned long)i]
+																	   target:nil
+																	 selector:NSSelectorFromString([NSString stringWithFormat:@"setColor%lu", (unsigned long)i])
+																   repeatRate:100.0];
+		if (shortcut) [jotManager addShortcutOption:shortcut];
+	}
+
     
     jotManager.unconnectedPressure = 0;
     jotManager.palmRejectorDelegate = canvasView;
